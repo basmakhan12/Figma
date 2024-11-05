8a0e53909d2f3dff121e004bec928e089e642cef
Basma khan <basmakhan672@gmail.com>
2024-11-05 17:49:55 +0500

    Add files via upload

---
+++ Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div className=' flex w-[1741.06px] h-[80px] bg-[#A29875]
+'>
+ <div className='flex justify-center h-[100px] w-[100px] mt-[20px] ml-[40px] font-semibold text-[#FFFFFF] text-xl font-serif
+'>MANZZARI</div>
+
+
+
+</div>
+    </div>
+  )
+}
+
+export default Header
+
